ccec464cc1c69815035cb9a8bcf441e31c9f40de
wiliamtrancoso <wileleh@gmail.com>
2016-05-04 19:35:47 -0300

    [master] [wiliam.trancoso]   \_> add setContentView in BaseActivity   \_> new method abstract in BaseActivity(getLayoutResId)   \_> ButterKnife.bind in onCreate

--- architecture/src/main/java/br/com/dalcim/architecture/activity/BaseActivity.java
+++ architecture/src/main/java/br/com/dalcim/architecture/activity/BaseActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 public abstract class BaseActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        ButterKnife.bind(this);
     }
+
+    protected abstract int getLayoutResId();
 }
