2d974fb6c7abbaebf26218ad10ef9478f652f9e4
wiliamtrancoso <wileleh@gmail.com>
2016-05-05 22:03:04 -0300

    [master] [wiliam.trancoso]   \_> startActivity overload   \_> abstraction ToastMakeText(...).show()

--- architecture/src/main/java/br/com/dalcim/architecture/activity/BaseActivity.java
+++ architecture/src/main/java/br/com/dalcim/architecture/activity/BaseActivity.java
@@ -1,13 +1,23 @@
 package br.com.dalcim.architecture.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity{
 
+    /* CONTRACT  */
+    protected abstract int getLayoutResId();
+
+    /* LIFE CICLE */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +25,18 @@ public abstract class BaseActivity extends AppCompatActivity{
         ButterKnife.bind(this);
     }
 
-    protected abstract int getLayoutResId();
+    /* NAVIGATION */
+    protected void startActivity(Class c){
+        startActivity(new Intent(this, c));
+    }
+
+    /* OUTPUTS MESSAGE */
+    protected void showLongMessage(String txt){
+        Toast.makeText(this, txt, Toast.LENGTH_LONG).show();
+    }
+
+    protected void showShortMessage(String txt){
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show();
+    }
+
 }
