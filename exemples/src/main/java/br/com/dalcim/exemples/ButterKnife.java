af4c033ec63dae7d35d860338303af5396ef397f
wiliamtrancoso <wileleh@gmail.com>
2016-05-06 17:15:05 -0300

    [master] [wiliam.trancoso]   \_> impl butterknife

--- exemples/src/main/java/br/com/dalcim/exemples/ButterKnife.java
+++ exemples/src/main/java/br/com/dalcim/exemples/ButterKnife.java
@@ -1,11 +1,35 @@
 package br.com.dalcim.exemples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import br.com.dalcim.architecture.activity.BaseActivity;
+import butterknife.Bind;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+import butterknife.OnEditorAction;
+import butterknife.OnFocusChange;
+import butterknife.OnItemClick;
+import butterknife.OnItemLongClick;
+import butterknife.OnLongClick;
+import butterknife.OnTextChanged;
+import butterknife.OnTouch;
 
 public class ButterKnife extends BaseActivity {
+
+    @Bind(R.id.bk_txt_hora)
+    TextView txtHora;
+
+    @Bind(R.id.bk_lst_exemplo)
+    ListView lstExemplo;
+
+    String[] lista = new String[]{"Item0", "Item1", "Item2"};
 
     @Override
     protected int getLayoutResId() { return R.layout.activity_butter_knife; }
@@ -13,5 +37,63 @@ public class ButterKnife extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        txtHora.setText(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" + Calendar.getInstance().get(Calendar.MINUTE));
+
+        lstExemplo.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista));
+    }
+
+    @OnCheckedChanged(R.id.bk_ckb_oncheckedchanged)
+    void onCheckedChanged(boolean checked) {
+        showShortMessage("onCheckChanged: " + (checked ? "Checked!" : "Unchecked!"));
+    }
+
+    @OnClick(R.id.bk_btn_onclick)
+    void onClick(){
+        showShortMessage("OnCkick");
+    }
+
+//    @OnEditorAction(R.id.bk_edt_oneditoraction)
+//    boolean onEditorAction(KeyEvent key) {
+//        showShortMessage("Pressed: " + key);
+//        return true;
+//    }
+
+    @OnFocusChange(R.id.bk_edt_onfocuschange)
+    void onFocusChanged(boolean focused) {
+        showShortMessage("onFocusChanged: " + (focused ? "Focused!" : "UnFocused!"));
+    }
+
+    @OnLongClick(R.id.bk_btn_onlongclick)
+    boolean onLongClick() {
+        showShortMessage("Long clicked!");
+        return true;
+    }
+
+    @OnTextChanged(R.id.bk_edt_ontextchanged)
+    void onTextChanged(CharSequence text) {
+        showShortMessage("Texto alterado: " + text);
+    }
+
+    @OnTextChanged(value = R.id.bk_edt_ontextchanged, callback = OnTextChanged.Callback.BEFORE_TEXT_CHANGED)
+    void onBeforeTextChanged(CharSequence text) {
+        showShortMessage("Texto anterio: " + text);
+    }
+
+    @OnTouch(R.id.bk_view_ontouch)
+    boolean onTouch() {
+        showShortMessage("Touched!");
+        return false;
+    }
+
+    @OnItemClick(R.id.bk_lst_exemplo)
+    void onItemClick(int position) {
+        showShortMessage("OnItemClick: " + position + " - " + lista[position]);
+    }
+
+    @OnItemLongClick(R.id.bk_lst_exemplo)
+    boolean  onItemLongClick(int position) {
+        showShortMessage("OnItemLongClick: " + position + " - " + lista[position]);
+        return true;
     }
 }
