462ee4a51a54c71160f9ccb80af49c17b7a7b035
wiliamtrancoso <wileleh@gmail.com>
2016-05-10 11:52:01 -0300

    [master] [wiliam.trancoso]   \_> add abstract BroadcastReceiver

---
+++ architecture/src/main/java/br/com/dalcim/architecture/receiver/BaseBroadcastReceiver.java
@@ -0,0 +1,8 @@
+package br.com.dalcim.architecture.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public abstract class BaseBroadcastReceiver extends BroadcastReceiver {
+}
