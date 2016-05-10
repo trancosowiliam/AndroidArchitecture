2e7e520df0d5bc20f132b37fe3354c8fd1d374c8
wiliamtrancoso <wileleh@gmail.com>
2016-05-10 11:55:30 -0300

    [master] [wiliam.trancoso]   \_> add necessary classes  for the implementation of eventBus

---
+++ examples/src/main/java/br/com/dalcim/examples/event/CarregadorEvent.java
@@ -0,0 +1,6 @@
+package br.com.dalcim.examples.event;
+
+import br.com.dalcim.architecture.event.BaseEventBus;
+
+public class CarregadorEvent extends BaseEventBus {
+}
---
+++ examples/src/main/java/br/com/dalcim/examples/receiver/CarregadorReceiver.java
@@ -0,0 +1,13 @@
+package br.com.dalcim.examples.receiver;
+
+import android.content.Context;
+import android.content.Intent;
+
+import br.com.dalcim.architecture.receiver.BaseBroadcastReceiver;
+
+public class CarregadorReceiver extends BaseBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
